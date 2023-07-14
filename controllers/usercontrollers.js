b51b6b07def6cd4fb639b3676dbc9ed178ef18ac
vgnsh1999 <vigneshvarathan@gmail.com>
2023-07-14 11:10:19 +0530

    Made the login backend api

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -19,11 +19,14 @@ const addUser = async(req,res,next)=>{
 const loginUser = async(req,res,next)=>{
     try{
         const {email,password} = req.body;
-        const user = await User.findOne({where:{email:email,password:password}});
-        if(user){
-           res.status(200).json({success:true,message:'User logged in successfully'});
-        } else {
-            res.status(404).json({success:false,message:'User was not found'});
+        const userEmail = await User.findOne({where:{email:email}});
+        const userPassword = await User.findOne({where:{password:password}});
+        if(userEmail && userPassword){
+            res.status(200).json({success:true,message:'User login sucessful'});
+        }else if(userEmail && !userPassword){
+           res.status(401).json({success:false,message:'User not authorized'});
+        } else if(!userEmail){
+            res.status(404).json({success:false,message:'User not found'});
         }
     } catch(error){
         console.log(JSON.stringify(error));
